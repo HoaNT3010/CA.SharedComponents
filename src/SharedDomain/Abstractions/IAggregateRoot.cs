a274df7f6186472197ba82317e99469ffcb158a6
Nguyễn Thái Hòa <hoa41300@gmail.com>
2025-12-25 21:20:51 +0700

    feat(shared-domain): add domain primitives
    Adding domain primitives served as basic building blocks of CA/DDD projects, including:
    - ValueObject base class.
    - IEntity interface and Entity base class.
    - IAggregateRoot interface and AggregateRoot base class.

---
+++ src/SharedDomain/Abstractions/IAggregateRoot.cs
@@ -0,0 +1,14 @@
+﻿namespace SharedDomain.Abstractions
+{
+    /// <summary>
+    /// Defines the contract for an Aggregate Root, which acts as the primary entry point for a cluster of domain objects.
+    /// </summary>
+    /// <typeparam name="TKey">The type used for the aggregate root's primary key.</typeparam>
+    /// <remarks>
+    /// This interface follows the <b>strict architectural approach</b>, ensuring that all 
+    /// aggregate roots are identified, fully auditable, and capable of raising domain events.
+    /// </remarks>
+    public interface IAggregateRoot<TKey> : IEntity<TKey>, IHasDomainEvents
+    {
+    }
+}
